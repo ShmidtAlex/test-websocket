7089c9f4cd1bdb1673404f769a7dbedf3fb0b214
ShmidtAlex <shmidt.alex@gmail.com>
2020-12-22 00:33:00 +0300

    routing with chartjsbased setted

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Interactive Desktop</router-link> |
       <router-link to="/websocket">Websocket Connectioin</router-link> |
-      <!-- <router-link to="/handsontable">Handsontable Data Visualization</router-link> -->
+      <router-link to="/chartjs">Chart.js Data Visualization</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -18,14 +18,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/WebsocketConnection.vue')
   },
-  // {
-  //   path: '/handsontable',
-  //   name: 'HandsonTableDataVisualization',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/HandsonTableDataVisualization.vue')
-  // }
+  {
+    path: '/chartjs',
+    name: 'ChartjsBased',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ChartjsBased.vue')
+  }
 ]
 
 const router = new VueRouter({
